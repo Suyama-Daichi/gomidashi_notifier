409d60368af55f631d789c80c0bb72f8f5d58b80
SuyamaDaichi <symdit@gmail.com>
2020-08-25 20:27:05 +0900

    機能実装
    ゴミ出しのロジックを実装

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,38 @@
 import { Context, APIGatewayEvent } from "aws-lambda";
 import axios from 'axios';
 import * as dotenv from "dotenv";
+import * as dateFns from 'date-fns';
 dotenv.config();
 
+const gomiDefine = {
+    0: null,
+    1: null,
+    2: '燃えるゴミ',
+    3: null,
+    4: '容器包装プラスチック・古紙',
+    5: '燃えるゴミ',
+    6: '不燃ごみ',
+}
+
 export async function handler(event: APIGatewayEvent, context?: Context): Promise<string> {
-    try {
-        return await makeRequest('OK');
-    } catch (error) {
-        return 'failed'
+    const currentDate = new Date();
+    const tommorowDate = dateFns.addDays(currentDate, 4);
+    const youbi = dateFns.getDay(tommorowDate);
+    let message = gomiDefine[youbi];
+
+    if (youbi === 6 && dateFns.getWeekOfMonth(tommorowDate) % 2 === 0) {
+        message = null
     }
+
+    if (message) {
+        try {
+            return await makeRequest(message);
+        } catch (error) {
+            return 'failed'
+        }
+    }
+
+    return 'NoGomiDay';
 }
 
 async function makeRequest(message: string) {
@@ -20,7 +44,7 @@ async function makeRequest(message: string) {
 
     return new Promise<any>(async (resolve, reject) => {
         baseRequest.post('', {
-            text: message
+            text: `明日は${message}の日です。`
         })
             .then(res => {
                 resolve(res)
